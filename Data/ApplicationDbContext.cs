ce45e1900210040d65876f7fd7907ef1c20c6507
KrzysztofPawlowski <krzysztofpeiv@o2.pl>
2023-05-16 19:49:27 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EFDemo.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFDemo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Person> Person { get; set; }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace EFDemo.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        [Column(TypeName = "varchar(100)")]
+        [DisplayName("Wprowadź imię")]
+        public string? FirstName { get; set; }
+
+        [DisplayName("Wprowadź rok")]
+        [Required(ErrorMessage = "Te pole jest wymagane.")]
+        [Range(1899, 2022, ErrorMessage = "Zakres 1899-2022")]
+        public int? Year { get; set; }
+
+        public string? Result { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
